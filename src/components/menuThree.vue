c1d9434910cd53e78a141938c82dc86840ef5ef7
hasubasora <330882327@qq.com>
2018-09-04 16:17:48 +0800

    8876787

--- src/components/menuThree.vue
+++ src/components/menuThree.vue
@@ -1,83 +1,67 @@
 <template>
-  <div>
-    <yd-navbar title="购物精选" style="z-index:2" height='.8rem' color="#f2f2f2" bgcolor="#ff5f17">
-      <router-link to="/productList" slot="left">
-        <yd-navbar-back-icon></yd-navbar-back-icon>
-      </router-link>
-      <router-link to="#" slot="right">
-      </router-link>
-    </yd-navbar>
-    <div class="layoutList">
-      <yd-scrollnav>
-        <yd-scrollnav-panel :label="item.CateName" v-for="(item, key) in GoodsList" :key="key">
-          <!-- 内容 -->
-          <!-- <p>{{item.CateName}}</p> -->
-          <yd-list :theme="1">
-            <yd-list-item v-for="(itemt,key) in item.LstProduct" :key="key" @click.native="GoItemDes(itemt.Id)">
-              <div slot="img" class="goodLists">
+    <div>
+        <yd-navbar title="购物精选" style="z-index:2" height='.8rem' color="#f2f2f2" bgcolor="#ff5f17">
+            <router-link to="/productList" slot="left">
+                <yd-navbar-back-icon color="#f2f2f2"></yd-navbar-back-icon>
+            </router-link>
+            <router-link to="#" slot="right">
+            </router-link>
+        </yd-navbar>
 
-                <div class="goodsImg">
-                  <!-- <span class="ProductTitle">{{itemt.ProductTitle}}</span> -->
-                  <img :src="itemt.ProductImg">
-                </div>
-                <div class="goodsTag">
-                  <span>优品购</span>精选优品包邮</div>
-                <h3>{{itemt.SubTitle}}</h3>
-                <span class="SaleCount">月销量{{itemt.SaleCount}}件</span>
-                <div class="SalePrice">
-                  <strong>￥{{itemt.SalePrice}}</strong><img src="../assets/Img/gwc.png" alt=""></div>
+        <yd-tab horizontal-scroll v-model="tab2" :callback="fn" :prevent-default="false" :item-click="itemClick">
+            <yd-tab-panel v-for="item in items" :label="item.CateName" :key="item.CateName">
+                <!-- 商品列表 -->
+                <yd-infinitescroll :callback="loadList" ref="infinitescrollDemo">
+                    <yd-list theme="3" slot="list">
+                        <yd-list-item v-for="(item, key) in rows" :key="key" @click.native="GoItemDes(item.Id)">
+                            <img slot="img" :src="item.ProductImg">
+                            <span slot="title">{{item.ProductTitle}}</span>
+                            <yd-list-other slot="other">
+                                <div>
+                                    <span class="demo-list-price">
+                                        <em>¥</em>{{item.SalePrice}}</span>
+                                    <span class="demo-list-del-price">{{item.Integral}}积分</span>
+                                </div>
+                                <div>月销量{{item.SaleCount}}件</div>
+                            </yd-list-other>
+                        </yd-list-item>
+                    </yd-list>
+                    <!-- 商品列表 end-->
+                    <!-- 数据全部加载完毕显示 -->
+                    <span slot="doneTip">啦啦啦，啦啦啦，没有数据啦~~</span>
 
-              </div>
+                    <!-- 加载中提示，不指定，将显示默认加载中图标 -->
+                    <img slot="loadingTip" src="http://static.ydcss.com/uploads/ydui/loading/loading10.svg" />
 
-            </yd-list-item>
-          </yd-list>
-          <!-- 内容 -->
-
-        </yd-scrollnav-panel>
-      </yd-scrollnav>
+                </yd-infinitescroll>
+            </yd-tab-panel>
+        </yd-tab>
     </div>
-  </div>
 </template>
 <script>
+import Vue from 'vue';
+import {InfiniteScroll} from 'vue-ydui/dist/lib.rem/infinitescroll';
+/* 使用px：import {InfiniteScroll} from 'vue-ydui/dist/lib.px/infinitescroll'; */
+
+Vue.component(InfiniteScroll.name, InfiniteScroll);
 export default {
-    name: "menuThree",
     data() {
         return {
-            GoodsList: [
-                // { label: "今日特惠" },
-                // { label: "烟灶推荐" },
-                // { label: "净水饮水推荐" },
-                // { label: "洗碗机推荐" },
-                // { label: "电热推荐" },
-                // { label: "燃热推荐" },
-                // { label: "消毒柜推荐" },
-                // { label: "嵌入式推荐" },
-                // { label: "商用电器" },
-                // { label: "活动说明" }
-            ]
+            tab2: 1,
+            page: 1,
+            pageSize: 10,
+            items: [
+                { CateName: "促销", content: " " },
+                { CateName: "精选", content: " " }
+            ],
+            rows: [],
+            Group_id: 0
         };
     },
     created() {
         console.log(this.$route.params.Group_id);
-
-        this.$axios({
-            method: "POST",
-            data: {
-                pageindex: 1,
-                pagesize: 10,
-                categoryid: this.$route.params.Group_id
-            },
-            url: this.$server.serverUrl + "/index/getcategoryproduct",
-            responseType: "json"
-        }).then(response => {
-            if (response.data.success == 400) {
-                this.$router.push({ name: "SignIn" });
-            }
-            if (response.data.success == 200) {
-                this.GoodsList = response.data.rows;
-                console.log(this.GoodsList);
-            }
-        });
+        this.getCategory();
+        this.getCategoryProduct(this.$route.params.Group_id);
     },
     methods: {
         GoItemDes(i) {
@@ -86,85 +70,99 @@ export default {
                 name: "GeneralItemDescription",
                 params: { Good_id: i }
             });
+        },
+        loadList() {
+            this.$axios({
+                method: "POST",
+                data: {
+                    pageindex: this.page,
+                    pagesize: this.pageSize,
+                    categoryid: this.Group_id
+                },
+                url: this.$server.serverUrl + "/index/getcategoryproduct"
+            }).then(response => {
+                console.log(response.data.rows);
+                const _list = response.data.rows;
+                this.rows = [...this.rows, ..._list];
+                if (_list.length < this.pageSize || this.page == 3) {
+                    /* 所有数据加载完毕 */
+                    this.$refs.infinitescrollDemo.$emit(
+                        "ydui.infinitescroll.loadedDone"
+                    );
+                    console.log(this.$refs.infinitescrollDemo);
+
+                    return;
+                }
+                console.log(this.$refs.infinitescrollDemo);
+
+                /* 单次请求数据完毕 */
+                this.$refs.infinitescrollDemo.$emit(
+                    "ydui.infinitescroll.finishLoad"
+                );
+
+                this.page++;
+            });
+        },
+        fn(label, key) {
+            console.log(label, key);
+        },
+        itemClick(key) {
+            this.Group_id = this.items[key].CategoryId;
+            console.log(this.items[key].CategoryId);
+            this.getCategoryProduct(this.items[key].CategoryId);
+
+            this.$dialog.loading.open("数据加载中");
+            setTimeout(() => {
+                this.tab2 = key;
+                this.$dialog.loading.close();
+                this.items[key].content = this.rows;
+            }, 1000);
+        },
+        getCategoryProduct(Group_id) {
+            this.$axios({
+                method: "POST",
+                data: {
+                    pageindex: 1,
+                    pagesize: 10,
+                    categoryid: Group_id
+                },
+                url: this.$server.serverUrl + "/index/getcategoryproduct",
+                responseType: "json"
+            }).then(response => {
+                if (response.data.success == 400) {
+                    this.$router.push({ name: "SignIn" });
+                }
+                if (response.data.success == 200) {
+                    this.rows = response.data.rows;
+                }
+            });
+        },
+
+        getCategory() {
+            this.$axios({
+                method: "POST",
+                data: {
+                    categoryid: this.$route.params.Group_id
+                },
+                url: this.$server.serverUrl + "/index/getcategory",
+                responseType: "json"
+            }).then(response => {
+                if (response.data.success == 400) {
+                    this.$router.push({ name: "SignIn" });
+                }
+                if (response.data.success == 200) {
+                    this.items = response.data.rows;
+                    // console.log(this.items);
+                    this.tab2 = 0;
+                }
+            });
         }
     }
 };
 </script>
 <style lang="scss">
-.layoutList {
-    //   position: relative;
-    .yd-scrollnav {
-        padding-top: 1rem;
-        z-index: 1;
-        .yd-list-img {
-            height: 5.3rem;
-            background: #fff;
-            padding: 0.2rem;
-            .goodLists {
-                height: 5rem;
-                .goodsImg {
-                    position: relative;
-                    text-align: center;
-                    > img {
-                        width: 3rem;
-                        height: 3rem;
-                        margin: 0;
-                        display: inline-block;
-                    }
-                    .ProductTitle {
-                        position: absolute;
-                        display: block;
-                        width: 100%;
-                        bottom: 0;
-                        left: 1px;
-                        text-align: center;
-                        padding: 0.1rem;
-                        // background: #f5f5f5;
-                    }
-                }
-                .goodsTag {
-                    font-size: 0.3rem;
-                    color: #ff5f17;
-                    > span {
-                        background: #ff5f17;
-                        color: #fff;
-                        padding: 0.05rem 0.1rem;
-                        font-size: 0.3rem;
-                        border-radius: 5px;
-                        margin-right: 0.1rem;
-                    }
-                }
-                h3 {
-                    font-size: 0.3rem;
-                    overflow: hidden;
-                    white-space: nowrap;
-                    line-height: 0.6rem;
-                    text-overflow: ellipsis;
-                }
-                .SaleCount {
-                    color: #777;
-                    font-size: 0.3rem;
-                }
-                .SalePrice {
-                    display: flex;
-                    justify-content: space-between;
-                    strong {
-                        font-size: 0.4rem;
-                    }
-                    > img {
-                        width: 0.8rem;
-                        height: 0.8rem;
-                        margin-top: -0.3rem;
-                        margin-right: 0.1rem;
-                    }
-                }
-            }
-        }
-    }
-}
-.yd-scrollnav-tab-item > li.yd-scrollnav-current > i:before {
-    content: "";
-    border-bottom: 2px solid red;
-    height: 30px;
+.demo-list-price {
+    font-size: 0.4rem;
+    color: #555;
 }
 </style>
--- static/temper.js
+++ static/temper.js
@@ -1,9 +1,7 @@
 module.exports = {
-
-    // serverUrl: 'http://192.168.1.112:811',
-    // serverUrl: 'http://192.168.0.200:818',
+    serverUrl: 'http://192.168.0.200:818',
     // serverUrl: 'http://192.168.1.115:809'
-    serverUrl: 'http://h5.huizhisuo.xyz',
+    // serverUrl: 'http://h5.huizhisuo.xyz',
     // serverUrl: 'http://h5.3dou.com',
 
 }
