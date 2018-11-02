5e6a17113a3e44c590d0a27b47748e65e0bc0ff2
hasubasora <330882327@qq.com>
2018-11-02 18:54:56 +0800

    123

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -79,6 +79,8 @@ export default {
         }
     },
     created() {
+    
+
         this.GetMyMessageNum();
         this.$axios({
             method: "POST",
--- src/main.js
+++ src/main.js
@@ -183,18 +183,20 @@ export const GetWeixinPay = (res) => {
         "paySign": _res.paySign //微信签名 
       },
       function (_res) {
+        console.log(_res);
+        console.log('_res==================');
         if (_res.err_msg == "get_brand_wcpay_request:ok") {
-          this.$router.push({
-            name: SuccessOrder,
+          router.push({
+            name: "SuccessOrder",
             query: {
               Isok: 1
             }
           });
           // 使用以上方式判断前端返回,微信团队郑重提示：SuccessOrder
           //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
-        }else{
-          this.$router.push({
-            name: SuccessOrder,
+        } else {
+          router.push({
+            name: "SuccessOrder",
             query: {
               Isok: 2
             }
